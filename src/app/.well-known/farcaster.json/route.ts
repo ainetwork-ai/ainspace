64195559f16cdd924262d0a2da01dc3fc6c553e7
minjaeli <minjaelee@comcom.ai>
2025-10-23 22:46:03 +0900

    set: farcaster.json

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -18,23 +18,22 @@ export async function GET() {
         },
         frame: withValidProperties({
             version: '1',
-            tags: ['miniapps', 'a2a', 'aiagents', 'society', 'chatagents'],
+            tags: ['social', 'messaging', 'aiagents'],
             name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
             homeUrl: process.env.NEXT_PUBLIC_URL,
             iconUrl: process.env.NEXT_PUBLIC_APP_ICON,
             ogTitle: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
             tagline: process.env.NEXT_PUBLIC_APP_TAGLINE,
-            imageUrl: process.env.NEXT_PUBLIC_APP_OG_IMAGE,
             subtitle: process.env.NEXT_PUBLIC_APP_SUBTITLE,
             ogImageUrl: process.env.NEXT_PUBLIC_APP_OG_IMAGE,
-            webhookUrl: process.env.NEXT_PUBLIC_APP_WEBHOOK_URL,
             buttonTitle: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
             description: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_DESCRIPTION,
             heroImageUrl: process.env.NEXT_PUBLIC_APP_OG_IMAGE,
             ogDescription: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_DESCRIPTION,
             splashImageUrl: process.env.NEXT_PUBLIC_APP_SPLASH_IMAGE,
             primaryCategory: process.env.NEXT_PUBLIC_APP_PRIMARY_CATEGORY,
-            splashBackgroundColor: process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR
+            splashBackgroundColor: process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR,
+            castShareUrl: process.env.NEXT_PUBLIC_URL
         }),
         baseBuilder: {
             allowedAddresses: ['0xFAB1fD44Df09dD1673e86fC999FaFCb6040a149A']
