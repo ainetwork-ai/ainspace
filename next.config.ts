fe9d2e1573f7d9f5ec47011d52c40089c9390e03
minjaeli <minjaelee@comcom.ai>
2025-10-23 22:50:30 +0900

    set: farcaster.json

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,23 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.public.blob.vercel-storage.com',
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**.public.blob.vercel-storage.com'
+            }
+        ]
+    },
+    async redirects() {
+        return [
+            {
+                source: '/.well-known/farcaster.json',
+                destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/019a114e-1634-dc03-113d-a160b96edd0a',
+                permanent: false
+            }
+        ];
+    }
 };
 
 export default nextConfig;
