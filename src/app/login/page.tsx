53083e0886acdf44839b475c622d59c3304b3987
minjaeli <minjaelee@comcom.ai>
2025-10-23 09:11:44 +0900

    set: wallet connect

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,54 @@
+'use client';
+
 import Image from 'next/image';
+import { Signature, SignatureButton } from '@coinbase/onchainkit/signature';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useAccount } from 'wagmi';
+import { cn, shortAddress } from '@/lib/utils';
 
 export default function LoginPage() {
+    const { address, isConnected } = useAccount();
+    const [nonce] = useState(() => Date.now().toString());
+    const message = useMemo(() => `Welcome to the AIN SPACE MiniApp!\n\nNonce: ${nonce}`, [nonce]);
+    const [showButton, setShowButton] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowButton(true);
+        }, 100);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    const handleSignature = useCallback(
+        async (signature: string) => {
+            if (!signature || !address) return;
+        },
+        [address, message]
+    );
+
+    const ConnectWalletButton = () => {
+        return (
+            <div className={cn('h-14 transition-all duration-700 ease-out', showButton ? 'opacity-100' : 'opacity-0')}>
+                <Signature message={message} onSuccess={handleSignature}>
+                    <SignatureButton
+                        label="Wallet Login"
+                        errorLabel="Try Again"
+                        successLabel={address ? shortAddress(address) : 'Wallet Login'}
+                        pendingLabel="Signing..."
+                        className={
+                            'z-10 inline-flex w-[180px] cursor-pointer items-center justify-center self-center rounded bg-[#7f4fe8]'
+                        }
+                    />
+                </Signature>
+            </div>
+        );
+    };
+
     return (
         <div className="flex h-screen w-full max-w-800 flex-col items-center justify-center gap-6 bg-[#B1E1FF]">
             <Image src="/login/logo.svg" alt="Login Background" width={190} height={108} />
-            <button className="z-10 inline-flex h-14 w-[190px] cursor-pointer items-center justify-center gap-2.5 rounded bg-[#7f4fe8] px-[18px] py-2">
-                <div data-layer="Import" className="Import justify-start font-['SF_Pro'] text-xl font-bold text-white">
-                    Wallet Login
-                </div>
-            </button>
+            <ConnectWalletButton />
             <Image src="/login/ainetwork.svg" alt="ainetwork" className="z-10" width={133} height={22} />
             <div className="h-10" />
             <div className="fixed bottom-0 flex w-full justify-center">
