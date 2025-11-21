f71767fbdb2216f21c9f81356561b5453274db1a
Yoojin Ko <db232100@gmail.com>
2025-11-21 17:11:51 +0900

    fix: style

--- src/components/BottomSheet.tsx
+++ src/components/BottomSheet.tsx
@@ -4,57 +4,58 @@ import { AgentInformation } from '@/stores';
 import { cn } from '@/lib/utils';
 
 interface BottomSheetProps {
-  open: boolean;
-  onOpenChange: (open: boolean) => void;
-  chatBoxRef: React.RefObject<ChatBoxRef>;
-  lastCommentary: string;
-  worldAgents: AgentInformation[];
-  currentThreadId: string | undefined;
-  threads: {
-    id: string;
-    message: string;
-    timestamp: Date;
-    agentsReached: number;
-    agentNames: string[];
-  }[];
-  onThreadSelect: (threadId: string | undefined) => void;
-  userId: string | null;
+    open: boolean;
+    onOpenChange: (open: boolean) => void;
+    chatBoxRef: React.RefObject<ChatBoxRef>;
+    lastCommentary: string;
+    worldAgents: AgentInformation[];
+    currentThreadId: string | undefined;
+    threads: {
+        id: string;
+        message: string;
+        timestamp: Date;
+        agentsReached: number;
+        agentNames: string[];
+    }[];
+    onThreadSelect: (threadId: string | undefined) => void;
+    userId: string | null;
 }
 
 export default function BottomSheet({
-  open,
-  onOpenChange,
-  chatBoxRef,
-  lastCommentary,
-  worldAgents,
-  currentThreadId,
-  threads,
-  onThreadSelect,
-  userId,
+    open,
+    onOpenChange,
+    chatBoxRef,
+    lastCommentary,
+    worldAgents,
+    currentThreadId,
+    threads,
+    onThreadSelect,
+    userId,
 }: BottomSheetProps) {
-  return (
-    <Drawer open={open} onOpenChange={onOpenChange} direction="bottom" >
-      <DrawerContent 
-        className={
-            cn(
-                "h-screen max-h-screen z-49 pb-[73px]",
-                "bg-black/50",
-            )}
+    return (
+        <Drawer open={open} onOpenChange={onOpenChange} direction="bottom" >
+            <DrawerContent 
+                className={
+                    cn(
+                        "h-screen max-h-screen z-49 pb-[73px]",
+                        "bg-black/50",
+                    )
+                }
             >
-          <DrawerHeader>
-            <DrawerTitle></DrawerTitle>
-          </DrawerHeader>
-          <ChatBox
-                ref={chatBoxRef}
-                className="h-screen"
-                aiCommentary={lastCommentary}
-                agents={worldAgents}
-                currentThreadId={currentThreadId}
-                threads={threads}
-                onThreadSelect={onThreadSelect}
-                userId={userId}
-            />
-      </DrawerContent>
-    </Drawer>
-  );
+                <DrawerHeader>
+                    <DrawerTitle />
+                </DrawerHeader>
+                <ChatBox
+                      ref={chatBoxRef}
+                      className="h-screen"
+                      aiCommentary={lastCommentary}
+                      agents={worldAgents}
+                      currentThreadId={currentThreadId}
+                      threads={threads}
+                      onThreadSelect={onThreadSelect}
+                      userId={userId}
+                />
+            </DrawerContent>
+        </Drawer>
+    );
 }
