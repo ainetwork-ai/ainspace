012e43b2c71eabcb1d374dd1c65b5075c85934d0
chan3785 <dirtyface1234@naver.com>
2025-12-16 14:55:13 +0900

    feat: update AgentTab to include additional token contracts
    - Added new ERC20 and ERC1155 token contracts for Ethereum and Base chains.
    - Updated existing contract addresses and included source information for on-chain and OpenSea collections.

--- src/components/tabs/AgentTab.tsx
+++ src/components/tabs/AgentTab.tsx
@@ -55,16 +55,31 @@ export default function AgentTab({
             //TODO (chanho): 컨트랙트 추가 필요 (erc1155, 그 외 홀더 체크 필요한 토큰들)
             // 이외에 다른 곳에서도 사용된다면 공용 함수로 refactoring 고려
             contracts: [
-                {
-                    chain: "ethereum",
+                {       
+                    chain: "ethereum",      //eth AIN
                     standard: "erc20",
-                    address: "0x3A810ff7211b40c4fA76205a14efe161615d0385"
+                    address: "0x3A810ff7211b40c4fA76205a14efe161615d0385",
+                    source: "onchain"
                 }, 
-                {
-                    chain: "base",
+                {   
+                    chain: "base",          //base AIN
                     standard: "erc20",
-                    address: "0xD4423795fd904D9B87554940a95FB7016f172773"
+                    address: "0xD4423795fd904D9B87554940a95FB7016f172773",
+                    source: "onchain"
                 },
+                {
+                    chain: "base",      //base sAIN
+                    standard: "erc20",
+                    address: "0x70e68AF68933D976565B1882D80708244E0C4fe9",
+            		source: "onchain" 
+                },
+                {
+                    chain: "ethereum",      //mini egg nft
+                    standard: "erc1155",
+                    address: "0x495f947276749Ce646f68AC8c248420045cb7b5e",
+                    source: "opensea",
+                    collection: "mysterious-minieggs"
+                }
             ]
         }
         try {
